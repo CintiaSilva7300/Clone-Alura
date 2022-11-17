62e6cee595edfc8ddb2e62532c879f6c61057d88
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-16 22:14:57 -0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -10,7 +9,7 @@ import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [AppComponent, NavBarComponent, CardComponent],
-  imports: [BrowserModule, RouterModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
